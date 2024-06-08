a173330234bfce6582d0a57d3624ae1915de8291
Jeorge Supero <supero.132634@surigao.sti.edu.ph>
2024-06-08 08:01:02 +0800

    Update sa recievable

--- CRM/Dashboard.Designer.cs
+++ CRM/Dashboard.Designer.cs
@@ -29,15 +29,7 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
-            Lbl_Status = new Label();
-            Lbl_ConsumerName = new Label();
-            Lbl_AcountNumber = new Label();
-            label6 = new Label();
-            label12 = new Label();
-            label13 = new Label();
-            label14 = new Label();
             btnFind = new Button();
-            Lbl_MeterNumber = new Label();
             dGridView_Consumer = new DataGridView();
             AccountNo = new DataGridViewTextBoxColumn();
             MeterNo = new DataGridViewTextBoxColumn();
@@ -59,80 +51,10 @@
             panel1.SuspendLayout();
             SuspendLayout();
             // 
-            // Lbl_Status
-            // 
-            Lbl_Status.AutoSize = true;
-            Lbl_Status.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
-            Lbl_Status.Location = new Point(148, 374);
-            Lbl_Status.Name = "Lbl_Status";
-            Lbl_Status.Size = new Size(60, 21);
-            Lbl_Status.TabIndex = 62;
-            Lbl_Status.Text = "label18";
-            // 
-            // Lbl_ConsumerName
-            // 
-            Lbl_ConsumerName.AutoSize = true;
-            Lbl_ConsumerName.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
-            Lbl_ConsumerName.Location = new Point(148, 252);
-            Lbl_ConsumerName.Name = "Lbl_ConsumerName";
-            Lbl_ConsumerName.Size = new Size(60, 21);
-            Lbl_ConsumerName.TabIndex = 60;
-            Lbl_ConsumerName.Text = "label16";
-            // 
-            // Lbl_AcountNumber
-            // 
-            Lbl_AcountNumber.AutoSize = true;
-            Lbl_AcountNumber.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
-            Lbl_AcountNumber.Location = new Point(148, 196);
-            Lbl_AcountNumber.Name = "Lbl_AcountNumber";
-            Lbl_AcountNumber.Size = new Size(60, 21);
-            Lbl_AcountNumber.TabIndex = 59;
-            Lbl_AcountNumber.Text = "label15";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
-            label6.Location = new Point(103, 353);
-            label6.Name = "label6";
-            label6.Size = new Size(59, 21);
-            label6.TabIndex = 58;
-            label6.Text = "Status:";
-            // 
-            // label12
-            // 
-            label12.AutoSize = true;
-            label12.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
-            label12.Location = new Point(38, 290);
-            label12.Name = "label12";
-            label12.Size = new Size(124, 21);
-            label12.TabIndex = 57;
-            label12.Text = "Meter Number:";
-            // 
-            // label13
-            // 
-            label13.AutoSize = true;
-            label13.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
-            label13.Location = new Point(27, 231);
-            label13.Name = "label13";
-            label13.Size = new Size(135, 21);
-            label13.TabIndex = 56;
-            label13.Text = "Consumer Name:";
-            // 
-            // label14
-            // 
-            label14.AutoSize = true;
-            label14.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
-            label14.Location = new Point(22, 173);
-            label14.Name = "label14";
-            label14.Size = new Size(140, 21);
-            label14.TabIndex = 55;
-            label14.Text = "Account Number:";
-            // 
             // btnFind
             // 
             btnFind.BackColor = Color.DodgerBlue;
-            btnFind.Location = new Point(747, 176);
+            btnFind.Location = new Point(595, 186);
             btnFind.Name = "btnFind";
             btnFind.Size = new Size(70, 27);
             btnFind.TabIndex = 53;
@@ -140,23 +62,13 @@
             btnFind.UseVisualStyleBackColor = false;
             btnFind.Click += btnFind_Click;
             // 
-            // Lbl_MeterNumber
-            // 
-            Lbl_MeterNumber.AutoSize = true;
-            Lbl_MeterNumber.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
-            Lbl_MeterNumber.Location = new Point(148, 311);
-            Lbl_MeterNumber.Name = "Lbl_MeterNumber";
-            Lbl_MeterNumber.Size = new Size(60, 21);
-            Lbl_MeterNumber.TabIndex = 61;
-            Lbl_MeterNumber.Text = "label17";
-            // 
             // dGridView_Consumer
             // 
             dGridView_Consumer.AllowUserToAddRows = false;
             dGridView_Consumer.AllowUserToDeleteRows = false;
             dGridView_Consumer.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dGridView_Consumer.Columns.AddRange(new DataGridViewColumn[] { AccountNo, MeterNo, Name, Status });
-            dGridView_Consumer.Location = new Point(373, 217);
+            dGridView_Consumer.Location = new Point(221, 227);
             dGridView_Consumer.Name = "dGridView_Consumer";
             dGridView_Consumer.ReadOnly = true;
             dGridView_Consumer.Size = new Size(640, 511);
@@ -192,7 +104,7 @@
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(375, 182);
+            label7.Location = new Point(223, 192);
             label7.Name = "label7";
             label7.Size = new Size(124, 15);
             label7.TabIndex = 49;
@@ -201,7 +113,7 @@
             // txt_search
             // 
             txt_search.BackColor = Color.LemonChiffon;
-            txt_search.Location = new Point(505, 179);
+            txt_search.Location = new Point(353, 189);
             txt_search.Name = "txt_search";
             txt_search.Size = new Size(236, 23);
             txt_search.TabIndex = 48;
@@ -255,7 +167,7 @@
             label10.AutoSize = true;
             label10.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label10.ForeColor = Color.WhiteSmoke;
-            label10.Location = new Point(627, 96);
+            label10.Location = new Point(525, 97);
             label10.Name = "label10";
             label10.Size = new Size(203, 32);
             label10.TabIndex = 1;
@@ -266,7 +178,7 @@
             lbl_OverAllTotal.AutoSize = true;
             lbl_OverAllTotal.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             lbl_OverAllTotal.ForeColor = Color.Red;
-            lbl_OverAllTotal.Location = new Point(860, 92);
+            lbl_OverAllTotal.Location = new Point(758, 93);
             lbl_OverAllTotal.Name = "lbl_OverAllTotal";
             lbl_OverAllTotal.Size = new Size(159, 37);
             lbl_OverAllTotal.TabIndex = 0;
@@ -284,7 +196,7 @@
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1052, 156);
+            panel1.Size = new Size(959, 156);
             panel1.TabIndex = 47;
             panel1.Paint += panel1_Paint;
             // 
@@ -293,7 +205,7 @@
             btnAddConsumer.BackColor = Color.DodgerBlue;
             btnAddConsumer.Image = (Image)resources.GetObject("btnAddConsumer.Image");
             btnAddConsumer.ImageAlign = ContentAlignment.MiddleRight;
-            btnAddConsumer.Location = new Point(184, 468);
+            btnAddConsumer.Location = new Point(38, 368);
             btnAddConsumer.Name = "btnAddConsumer";
             btnAddConsumer.Size = new Size(136, 50);
             btnAddConsumer.TabIndex = 66;
@@ -307,7 +219,7 @@
             button4.BackColor = Color.DodgerBlue;
             button4.Image = (Image)resources.GetObject("button4.Image");
             button4.ImageAlign = ContentAlignment.MiddleRight;
-            button4.Location = new Point(184, 524);
+            button4.Location = new Point(38, 424);
             button4.Name = "button4";
             button4.Size = new Size(136, 50);
             button4.TabIndex = 68;
@@ -320,7 +232,7 @@
             button5.BackColor = Color.DodgerBlue;
             button5.Image = (Image)resources.GetObject("button5.Image");
             button5.ImageAlign = ContentAlignment.MiddleRight;
-            button5.Location = new Point(42, 524);
+            button5.Location = new Point(38, 483);
             button5.Name = "button5";
             button5.Size = new Size(136, 50);
             button5.TabIndex = 69;
@@ -332,23 +244,16 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1052, 778);
+            ClientSize = new Size(959, 778);
             Controls.Add(button5);
             Controls.Add(button4);
             Controls.Add(btnAddConsumer);
-            Controls.Add(Lbl_Status);
-            Controls.Add(Lbl_ConsumerName);
-            Controls.Add(Lbl_AcountNumber);
-            Controls.Add(label6);
-            Controls.Add(label12);
-            Controls.Add(label13);
-            Controls.Add(label14);
             Controls.Add(btnFind);
-            Controls.Add(Lbl_MeterNumber);
             Controls.Add(dGridView_Consumer);
             Controls.Add(label7);
             Controls.Add(txt_search);
             Controls.Add(panel1);
+            Name = "Dashboard";
             Text = "Dashboard";
             Load += Dashboard_Load;
             ((System.ComponentModel.ISupportInitialize)dGridView_Consumer).EndInit();
@@ -359,16 +264,7 @@
         }
 
         #endregion
-
-        private Label Lbl_Status;
-        private Label Lbl_ConsumerName;
-        private Label Lbl_AcountNumber;
-        private Label label6;
-        private Label label12;
-        private Label label13;
-        private Label label14;
         private Button btnFind;
-        private Label Lbl_MeterNumber;
         private DataGridView dGridView_Consumer;
         private Label label7;
         private TextBox txt_search;
--- CRM/Recievables.Designer.cs
+++ CRM/Recievables.Designer.cs
@@ -41,6 +41,16 @@
             label10 = new Label();
             lbl_OverAllTotal = new Label();
             Lbl_RecAmount = new Label();
+            Lbl_Status = new Label();
+            Lbl_ConsumerName = new Label();
+            Lbl_AcountNumber = new Label();
+            label6 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            Lbl_MeterNumber = new Label();
+            label1 = new Label();
+            label2 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -50,7 +60,7 @@
             dataGridView1.AllowUserToDeleteRows = false;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Month, Amount, Penalty, Total });
-            dataGridView1.Location = new Point(223, 89);
+            dataGridView1.Location = new Point(427, 78);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.ReadOnly = true;
             dataGridView1.Size = new Size(462, 478);
@@ -86,7 +96,7 @@
             btn_UpdateRecievable.BackColor = Color.DarkTurquoise;
             btn_UpdateRecievable.Image = (Image)resources.GetObject("btn_UpdateRecievable.Image");
             btn_UpdateRecievable.ImageAlign = ContentAlignment.MiddleRight;
-            btn_UpdateRecievable.Location = new Point(46, 249);
+            btn_UpdateRecievable.Location = new Point(165, 446);
             btn_UpdateRecievable.Name = "btn_UpdateRecievable";
             btn_UpdateRecievable.Size = new Size(102, 45);
             btn_UpdateRecievable.TabIndex = 76;
@@ -99,7 +109,7 @@
             btn_DeleteRecievable.BackColor = Color.DarkTurquoise;
             btn_DeleteRecievable.Image = (Image)resources.GetObject("btn_DeleteRecievable.Image");
             btn_DeleteRecievable.ImageAlign = ContentAlignment.MiddleRight;
-            btn_DeleteRecievable.Location = new Point(46, 300);
+            btn_DeleteRecievable.Location = new Point(165, 511);
             btn_DeleteRecievable.Name = "btn_DeleteRecievable";
             btn_DeleteRecievable.Size = new Size(102, 45);
             btn_DeleteRecievable.TabIndex = 75;
@@ -113,7 +123,7 @@
             btn_AddAmtPen.BackColor = Color.DarkTurquoise;
             btn_AddAmtPen.Image = (Image)resources.GetObject("btn_AddAmtPen.Image");
             btn_AddAmtPen.ImageAlign = ContentAlignment.MiddleRight;
-            btn_AddAmtPen.Location = new Point(46, 198);
+            btn_AddAmtPen.Location = new Point(165, 383);
             btn_AddAmtPen.Name = "btn_AddAmtPen";
             btn_AddAmtPen.Size = new Size(102, 45);
             btn_AddAmtPen.TabIndex = 74;
@@ -125,11 +135,11 @@
             // label7
             // 
             label7.AutoSize = true;
-            label7.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Font = new Font("Segoe UI Semibold", 20.25F, FontStyle.Bold | FontStyle.Italic);
             label7.ForeColor = Color.White;
             label7.Location = new Point(12, 18);
             label7.Name = "label7";
-            label7.Size = new Size(167, 37);
+            label7.Size = new Size(158, 37);
             label7.TabIndex = 77;
             label7.Text = "Recievables";
             // 
@@ -138,18 +148,18 @@
             label10.AutoSize = true;
             label10.Font = new Font("Segoe UI Semibold", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             label10.ForeColor = Color.WhiteSmoke;
-            label10.Location = new Point(267, 28);
+            label10.Location = new Point(427, 18);
             label10.Name = "label10";
-            label10.Size = new Size(228, 37);
+            label10.Size = new Size(258, 37);
             label10.TabIndex = 79;
-            label10.Text = "TOTAL AMOUNT:";
+            label10.Text = "OVERALL AMOUNT:";
             // 
             // lbl_OverAllTotal
             // 
             lbl_OverAllTotal.AutoSize = true;
             lbl_OverAllTotal.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             lbl_OverAllTotal.ForeColor = Color.Red;
-            lbl_OverAllTotal.Location = new Point(471, 19);
+            lbl_OverAllTotal.Location = new Point(661, 9);
             lbl_OverAllTotal.Name = "lbl_OverAllTotal";
             lbl_OverAllTotal.Size = new Size(0, 37);
             lbl_OverAllTotal.TabIndex = 78;
@@ -159,19 +169,139 @@
             Lbl_RecAmount.AutoSize = true;
             Lbl_RecAmount.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             Lbl_RecAmount.ForeColor = Color.Red;
-            Lbl_RecAmount.Location = new Point(501, 28);
+            Lbl_RecAmount.Location = new Point(691, 18);
             Lbl_RecAmount.Name = "Lbl_RecAmount";
             Lbl_RecAmount.Size = new Size(159, 37);
             Lbl_RecAmount.TabIndex = 80;
             Lbl_RecAmount.Text = "568,973.99";
             Lbl_RecAmount.Click += Lbl_RecAmount_Click;
             // 
+            // Lbl_Status
+            // 
+            Lbl_Status.AutoSize = true;
+            Lbl_Status.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold);
+            Lbl_Status.ForeColor = Color.WhiteSmoke;
+            Lbl_Status.Location = new Point(165, 279);
+            Lbl_Status.Name = "Lbl_Status";
+            Lbl_Status.Size = new Size(72, 25);
+            Lbl_Status.TabIndex = 88;
+            Lbl_Status.Text = "label18";
+            // 
+            // Lbl_ConsumerName
+            // 
+            Lbl_ConsumerName.AutoSize = true;
+            Lbl_ConsumerName.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold);
+            Lbl_ConsumerName.ForeColor = Color.WhiteSmoke;
+            Lbl_ConsumerName.Location = new Point(165, 157);
+            Lbl_ConsumerName.Name = "Lbl_ConsumerName";
+            Lbl_ConsumerName.Size = new Size(72, 25);
+            Lbl_ConsumerName.TabIndex = 86;
+            Lbl_ConsumerName.Text = "label16";
+            // 
+            // Lbl_AcountNumber
+            // 
+            Lbl_AcountNumber.AutoSize = true;
+            Lbl_AcountNumber.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold);
+            Lbl_AcountNumber.ForeColor = Color.WhiteSmoke;
+            Lbl_AcountNumber.Location = new Point(165, 101);
+            Lbl_AcountNumber.Name = "Lbl_AcountNumber";
+            Lbl_AcountNumber.Size = new Size(72, 25);
+            Lbl_AcountNumber.TabIndex = 85;
+            Lbl_AcountNumber.Text = "label15";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold);
+            label6.ForeColor = Color.WhiteSmoke;
+            label6.Location = new Point(120, 258);
+            label6.Name = "label6";
+            label6.Size = new Size(69, 25);
+            label6.TabIndex = 84;
+            label6.Text = "Status:";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold);
+            label12.ForeColor = Color.WhiteSmoke;
+            label12.Location = new Point(55, 195);
+            label12.Name = "label12";
+            label12.Size = new Size(146, 25);
+            label12.TabIndex = 83;
+            label12.Text = "Meter Number:";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold);
+            label13.ForeColor = Color.WhiteSmoke;
+            label13.Location = new Point(44, 136);
+            label13.Name = "label13";
+            label13.Size = new Size(161, 25);
+            label13.TabIndex = 82;
+            label13.Text = "Consumer Name:";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold);
+            label14.ForeColor = Color.WhiteSmoke;
+            label14.Location = new Point(39, 78);
+            label14.Name = "label14";
+            label14.Size = new Size(165, 25);
+            label14.TabIndex = 81;
+            label14.Text = "Account Number:";
+            // 
+            // Lbl_MeterNumber
+            // 
+            Lbl_MeterNumber.AutoSize = true;
+            Lbl_MeterNumber.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold);
+            Lbl_MeterNumber.ForeColor = Color.WhiteSmoke;
+            Lbl_MeterNumber.Location = new Point(165, 216);
+            Lbl_MeterNumber.Name = "Lbl_MeterNumber";
+            Lbl_MeterNumber.Size = new Size(71, 25);
+            Lbl_MeterNumber.TabIndex = 87;
+            Lbl_MeterNumber.Text = "label17";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold);
+            label1.ForeColor = Color.WhiteSmoke;
+            label1.Location = new Point(712, 559);
+            label1.Name = "label1";
+            label1.Size = new Size(58, 25);
+            label1.TabIndex = 89;
+            label1.Text = "Total:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Red;
+            label2.Location = new Point(775, 559);
+            label2.Name = "label2";
+            label2.Size = new Size(101, 25);
+            label2.TabIndex = 90;
+            label2.Text = "568,973.99";
+            // 
             // Recievables
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DarkOrange;
-            ClientSize = new Size(735, 579);
+            ClientSize = new Size(935, 614);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(Lbl_Status);
+            Controls.Add(Lbl_ConsumerName);
+            Controls.Add(Lbl_AcountNumber);
+            Controls.Add(label6);
+            Controls.Add(label12);
+            Controls.Add(label13);
+            Controls.Add(label14);
+            Controls.Add(Lbl_MeterNumber);
             Controls.Add(Lbl_RecAmount);
             Controls.Add(label10);
             Controls.Add(lbl_OverAllTotal);
@@ -201,5 +331,15 @@
         private Label label10;
         private Label lbl_OverAllTotal;
         private Label Lbl_RecAmount;
+        private Label Lbl_Status;
+        private Label Lbl_ConsumerName;
+        private Label Lbl_AcountNumber;
+        private Label label6;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label Lbl_MeterNumber;
+        private Label label1;
+        private Label label2;
     }
 }
